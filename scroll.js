6e0ee028947444cec21c5af5eff02e5879754125
santh0shh <mr.santhosh116@gmail.com>
2024-07-05 19:23:44 +0530

    Update scroll.js

--- scroll.js
+++ scroll.js
@@ -42,8 +42,8 @@ window.onscroll = function(){
         for(i=0;i<11;i++){
             document.getElementsByClassName('temp')[i].style.margin = '30px';
         }
-        alert(curpos  + window.innerHeight + " = " + document.documentElement.scrollHeight);
-        alert(h);
+        // alert(curpos  + window.innerHeight + " = " + document.documentElement.scrollHeight);
+        // alert(h);
     }
 
     const documentHeight = Math.max(
